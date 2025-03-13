de56ba1eb54a875a0013010046e71d93addbc51c
alvinow <alvinowijaksana@gmail.com>
2025-03-14 00:48:30 +0700

    #320 commit

--- lib/modules/base/vwnode/vwnodecontent/vwnodecontent.g.dart
+++ lib/modules/base/vwnode/vwnodecontent/vwnodecontent.g.dart
@@ -6,39 +6,79 @@ part of 'vwnodecontent.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-VwNodeContent _$VwNodeContentFromJson(Map<String, dynamic> json) =>
-    VwNodeContent(
-      classEncodedJson: json['classEncodedJson'] == null
-          ? null
-          : VwClassEncodedJson.fromJson(
-              json['classEncodedJson'] as Map<String, dynamic>),
-      linkFileStorageId: json['linkFileStorageId'] as String?,
-      linkbasemodel: json['linkbasemodel'] == null
-          ? null
-          : VwLinkBaseModel.fromJson(
-              json['linkbasemodel'] as Map<String, dynamic>),
-      linkNode: json['linkNode'] == null
-          ? null
-          : VwLinkNode.fromJson(json['linkNode'] as Map<String, dynamic>),
-      linkRowCollection: json['linkRowCollection'] == null
-          ? null
-          : VwLinkRowCollection.fromJson(
-              json['linkRowCollection'] as Map<String, dynamic>),
-      renderedDate: json['renderedDate'] == null
-          ? null
-          : DateTime.parse(json['renderedDate'] as String),
-      contentContext: json['contentContext'] == null
-          ? null
-          : VwContentContext.fromJson(
-              json['contentContext'] as Map<String, dynamic>),
-      rowData: json['rowData'] == null
-          ? null
-          : VwRowData.fromJson(json['rowData'] as Map<String, dynamic>),
-      fileStorage: json['fileStorage'] == null
-          ? null
-          : VwFileStorage.fromJson(json['fileStorage'] as Map<String, dynamic>),
-      tag: json['tag'] as String?,
-    );
+VwNodeContent _$VwNodeContentFromJson(Map<String, dynamic> json) {
+      try{
+            return  VwNodeContent(
+                  classEncodedJson: json['classEncodedJson'] == null
+                      ? null
+                      : VwClassEncodedJson.fromJson(
+                      json['classEncodedJson'] as Map<String, dynamic>),
+                  linkFileStorageId: json['linkFileStorageId'] as String?,
+                  linkbasemodel: json['linkbasemodel'] == null
+                      ? null
+                      : VwLinkBaseModel.fromJson(
+                      json['linkbasemodel'] as Map<String, dynamic>),
+                  linkNode: json['linkNode'] == null
+                      ? null
+                      : VwLinkNode.fromJson(json['linkNode'] as Map<String, dynamic>),
+                  linkRowCollection: json['linkRowCollection'] == null
+                      ? null
+                      : VwLinkRowCollection.fromJson(
+                      json['linkRowCollection'] as Map<String, dynamic>),
+                  renderedDate: json['renderedDate'] == null
+                      ? null
+                      : DateTime.parse(json['renderedDate'] as String),
+                  contentContext: json['contentContext'] == null
+                      ? null
+                      : VwContentContext.fromJson(
+                      json['contentContext'] as Map<String, dynamic>),
+                  rowData: json['rowData'] == null
+                      ? null
+                      : VwRowData.fromJson(json['rowData'] as Map<String, dynamic>),
+                  fileStorage: json['fileStorage'] == null
+                      ? null
+                      : VwFileStorage.fromJson(json['fileStorage'] as Map<String, dynamic>),
+                  tag: json['tag'] as String?,
+            );
+      }
+      catch(error)
+      {
+            print("Error catched on VwNodeContentFromJson(Map<String, dynamic> json) "+error.toString());
+      }
+      return  VwNodeContent(
+            classEncodedJson: json['classEncodedJson'] == null
+                ? null
+                : VwClassEncodedJson.fromJson(
+                json['classEncodedJson'] as Map<String, dynamic>),
+            linkFileStorageId: json['linkFileStorageId'] as String?,
+            linkbasemodel: json['linkbasemodel'] == null
+                ? null
+                : VwLinkBaseModel.fromJson(
+                json['linkbasemodel'] as Map<String, dynamic>),
+            linkNode: json['linkNode'] == null
+                ? null
+                : VwLinkNode.fromJson(json['linkNode'] as Map<String, dynamic>),
+            linkRowCollection: json['linkRowCollection'] == null
+                ? null
+                : VwLinkRowCollection.fromJson(
+                json['linkRowCollection'] as Map<String, dynamic>),
+            renderedDate: json['renderedDate'] == null
+                ? null
+                : DateTime.parse(json['renderedDate'] as String),
+            contentContext: json['contentContext'] == null
+                ? null
+                : VwContentContext.fromJson(
+                json['contentContext'] as Map<String, dynamic>),
+            rowData: json['rowData'] == null
+                ? null
+                : VwRowData.fromJson(json['rowData'] as Map<String, dynamic>),
+            fileStorage: json['fileStorage'] == null
+                ? null
+                : VwFileStorage.fromJson(json['fileStorage'] as Map<String, dynamic>),
+            tag: json['tag'] as String?,
+      );
+}
+
 
 Map<String, dynamic> _$VwNodeContentToJson(VwNodeContent instance) =>
     <String, dynamic>{
